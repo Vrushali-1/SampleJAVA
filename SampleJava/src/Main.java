8ada74599e313fd6b547145865c0fdc0e6523d6e
Vrushali <vrushalipatil773@gmail.com>
2020-07-08 17:25:37 +0530

    committed by Vrushali

---
+++ SampleJava/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	     String [] name= {"Vrushali"};
+	     
+	     for(int i=0;i<name.length;i++) {
+	    	 System.out.println("1st: "+name[i]);
+	     }
+
+	}
+
+}
