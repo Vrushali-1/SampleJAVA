fb3f528e7faa77617144bbc7572852e61f1775f6
Vrushali <vrushalipatil773@gmail.com>
2020-07-08 17:28:01 +0530

    added omkar in the array

--- SampleJava/src/Main.java
+++ SampleJava/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	     String [] name= {"Vrushali"};
+	     String [] name= {"Vrushali","Omkar"};
 	     
 	     for(int i=0;i<name.length;i++) {
 	    	 System.out.println("1st: "+name[i]);
